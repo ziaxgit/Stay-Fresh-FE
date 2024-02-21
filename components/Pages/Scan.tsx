feec532b7fdca3ea897814c015f52e2868354489
ziaxgit <112583896+ziaxgit@users.noreply.github.com>
2024-02-21 14:11:38 +0000

    added expo-camera, still working on it

--- components/Pages/Scan.tsx
+++ components/Pages/Scan.tsx
@@ -1,16 +1,53 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { Button, Image, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
-const Scan = () => {
-  return <Text>Scan Here!</Text>;
-};
+export default function Scan() {
+  const [image, setImage] = useState(null);
 
-export default Scan;
+  const takePicture = async () => {
+    const [status, requestPermission] = ImagePicker.useCameraPermissions();
 
-const styles = StyleSheet.create({
-  header: {
-    fontSize: 20,
-    padding: 23,
-    flex: 1,
-  },
-});
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false, // Disable cropping
+      quality: 1,
+    });
+
+    console.log(result, "<<<< RESULT"); // Log the result to see its structure
+    console.log("====================================");
+    console.log("test log");
+    setImage(result.assets[0].uri);
+    console.log("====================================");
+    console.log(image);
+    console.log("====================================");
+  };
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: false, // Disable cropping
+      quality: 1,
+    });
+    setImage(result.assets[0].uri);
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Button title="Take Picture" onPress={takePicture} />
+      <Button title="Pick Image from Gallery" onPress={pickImage} />
+
+      {image && (
+        <Image
+          source={{ uri: image }}
+          style={{
+            width: 400,
+            height: 400,
+            marginTop: 20,
+            objectFit: "contain",
+          }}
+        />
+      )}
+    </View>
+  );
+}
