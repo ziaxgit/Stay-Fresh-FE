8e80a81286583b23dcd800a0f315deb838a7f6c3
emran-islam <emran_islam@hotmail.co.uk>
2024-02-26 13:55:27 +0000

    further styling for profile page

--- components/Pages/Profile.tsx
+++ components/Pages/Profile.tsx
@@ -26,19 +26,35 @@ const Profile = () => {
   }, []);
 
   return (
-    <View className="flex-1 items-center mt-6">
-      <Text className="text-3xl mb-5">PROFILE</Text>
-      <Image
-        className="rounded-full w-36 h-36"
-        source={{
-          uri: "https://ca.slack-edge.com/T01KPE0QGCD-U066BE21ALW-g582122507ca-512",
-        }}
-      />
-      <Text className="text-center mt-5 text-lg">Username: {userName}</Text>
-      <Text className="text-center mt-1 text-lg">
-        Email: smith01@hotmail.com
-      </Text>
-      <Text className="text-center mt-1 text-lg">Home: {homeName}</Text>
+    <View className="flex-1 items-center  ">
+      <View className="bg-white rounded-lg mt-6 shadow-lg py-6 px-20 flex flex-col items-center">
+        <Text className="text-3xl mb-4 font-bold text-800">PROFILE</Text>
+        <Image
+          className="rounded-full w-36 h-36 mb-4"
+          source={{
+            uri: "https://ca.slack-edge.com/T01KPE0QGCD-U066BE21ALW-g582122507ca-512",
+          }}
+        />
+        <Text>
+          <Text className="flex items-center text-lg font-semibold text-gray-700">
+            Username:
+          </Text>
+          <Text className="text-lg text-900"> {userName}</Text>
+        </Text>
+        <Text>
+          <Text className="flex items-center text-lg font-semibold text-gray-700">
+            Email:
+          </Text>
+          <Text className="text-lg text-900"> smith01@hotmail.com</Text>
+        </Text>
+        <Text>
+          {" "}
+          <Text className="flex items-center text-lg font-semibold text-gray-700">
+            Home:
+          </Text>
+          <Text className="text-center mt-1 text-lg"> {homeName}</Text>
+        </Text>
+      </View>
     </View>
   );
 };
