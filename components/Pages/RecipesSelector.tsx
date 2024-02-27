9597a5fcd9e68739566cbda0460821bb56160e28
drewanderson201 <drewanderson201@gmail.com>
2024-02-27 16:02:08 +0000

    add get recipe api and recipes nav page

---
+++ components/Pages/RecipesSelector.tsx
@@ -0,0 +1,22 @@
+
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ImageBackground,
+  Dimensions,
+  ScrollView,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+
+export default function RecipesSelector(){
+
+      return (
+        <View>
+            <Text>This is recipe selector</Text>
+        </View>
+      )
+
+}
+
